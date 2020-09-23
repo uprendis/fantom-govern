22a8765df79c938f633f238e5f12799e74a1d5c8
Egor Lysenko <uprendis@gmail.com>
2020-09-23 19:13:17 +0700

    refactor Governable interface

--- contracts/governance/Governance.sol
+++ contracts/governance/Governance.sol
@@ -340,8 +340,7 @@ contract Governance is Initializable, ReentrancyGuard, GovernanceSettings, Versi
     function _processNewVote(uint256 proposalID, address voterAddr, address delegatedTo, uint256[] memory choices) internal returns (uint256) {
         if (delegatedTo == voterAddr) {
             // voter isn't a delegator
-            (uint256 ownVotingWeight, uint256 delegatedMeVotingWeight) = governableContract.getWeight(voterAddr);
-            uint256 weight = ownVotingWeight.add(delegatedMeVotingWeight);
+            uint256 weight = governableContract.getReceivedWeight(voterAddr);
             uint256 overridden = overriddenWeight[voterAddr][proposalID];
             if (weight > overridden) {
                 weight -= overridden;
@@ -355,7 +354,7 @@ contract Governance is Initializable, ReentrancyGuard, GovernanceSettings, Versi
             return weight;
         } else {
             // votes through one of delegations, overrides previous vote of "delegatedTo" (if any)
-            uint256 delegatedWeight = governableContract.getDelegatedWeight(voterAddr, delegatedTo);
+            uint256 delegatedWeight = governableContract.getWeight(voterAddr, delegatedTo);
             // reduce weight of vote of "delegatedTo" (if any)
             overrideDelegationWeight(delegatedTo, proposalID, delegatedWeight);
             _recountVote(delegatedTo, delegatedTo, proposalID);
